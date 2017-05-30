8d1520128f425c0583a963f33263ec618c1d5c95
macster110 <macster110@gmail.com>
2017-05-30 15:15:52 +0100

    Arduino files

---
+++ SoundTrap/proMicro_Digitial_Pin_test/proMicro_Digitial_Pin_test.ino
@@ -0,0 +1,20 @@
+int ledPin = 10;                 // LED connected to digital pin 13
+
+#define Pin13LED         13
+
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);      // sets the digital pin as output
+}
+
+void loop()
+{
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+       TXLED0;
+  delay(4000);                  // waits for a second
+  digitalWrite(ledPin, LOW);    // sets the LED off
+        TXLED1;
+  delay(4000);                  // waits for a second
+}
+
